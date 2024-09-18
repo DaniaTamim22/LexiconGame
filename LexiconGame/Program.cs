ab85e7094b4de5d96ce2bfbe45b4e5ab2c77e9d7
Dania Tamim <dania.tamim@live.se>
2024-09-18 14:12:39 +0200

    Add project files.

---
+++ LexiconGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LexiconGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
